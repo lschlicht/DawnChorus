93d5cef00bda19cca5666e9e92c679e8c9533ee8
Lotte Schlicht <lschlicht@orn.mpg.de>
2023-10-17 12:15:34 +0200

    Update 000_startupMessage.r

--- R/000_startupMessage.r
+++ R/000_startupMessage.r
@@ -4,8 +4,8 @@
 'A dawn and dusk chorus will emerge if males sing in the absence of their mate'\n
 
 HOW TO USE:
-Dataset 1: 'ActivityDataPerMimute' Contains male song and female box visits per minute
-Dataset 2: 'ActivityDataPerMimute' Contains male song and female box visits per period (before, during or after female box visit)
+Dataset 1: 'ActivityDataPerMinute' Contains male song and female box visits per minute
+Dataset 2: 'ActivityDataPerPeriod' Contains male song and female box visits per period (before, during or after female box visit)
 To load the datasets type
 > data(DawnChorus)\r\n
 A description of the dataset can be found at\n
